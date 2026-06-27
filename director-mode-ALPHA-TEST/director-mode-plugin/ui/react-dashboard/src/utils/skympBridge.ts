34af945ec737a6463e6cbf84b86c3e6690990bda
XanakinO <112834410+XanakinO@users.noreply.github.com>
2026-06-26 21:53:25 -0400

    Delete director-mode-ALPHA-TEST directory

--- director-mode-ALPHA-TEST/director-mode-plugin/ui/react-dashboard/src/utils/skympBridge.ts
+++
@@ -1,8 +0,0 @@
-const sendToSkymp = (type: string, data: any) => {
-  console.log('Sending to SkyMP:', type, data);
-  if ((window as any).skyrimPlatform) {
-    (window as any).skyrimPlatform.sendMessage({ type, data });
-  }
-};
-
-export { sendToSkymp };
